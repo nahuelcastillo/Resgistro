0499c4c88c0c2939b014d33561c1ebca6e4995d3
nahuel.castillo44@gmail,com <nahuel.castillo44@gmail.com>
2023-08-11 14:36:15 -0300

    vOficial

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,31 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+    
+
+document.getElementById("regBtn").addEventListener("click", function (){
+    const checkboxInput = document.querySelectorAll("#terminos");
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var email = document.getElementById("email").value;
+    const password1= document.querySelector("#password1").value;
+    const password2= document.querySelector("#password2").value;
+
+    if(email )
+
+    if (nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "" || !checkboxInput) {
+      showAlertError();
+      return;
+    }
+    
+
+    if (password1.lenght >= 6 || password1 === password2){
+        showAlertSuccess();
+    } else {
+        showAlertError();
+    }
+
+})
+
+
